c8c812bf964e91420a4e76d8381ceb4ab96c3a43
Juha Halmu <jhalmu@users.noreply.github.com>
2022-09-13 17:32:41 +0300

    Some html and css fixes

--- src/config/settings.js
+++
@@ -1,8 +0,0 @@
-export default {
-	title: `Stackoverlove - Juha Halmu`,
-	description: `Koodausta, www-sivuja, kerrontaa`,
-	url: `https://stackoverlove.me`,
-	name: `Juha`,
-	addres: `Helsinki, FI`,
-	analytics: '' // analytics code
-};
